ffec4ae914bde4bb4095440f6bf73806fc478c4d
ollikolli <olli.a.korhonen@aalto.fi>
2023-10-04 15:50:03 +0300

    Ronaldottaminen toimivaksi

--- src/main/scala/s1/telegrambots/YourBot.scala
+++ src/main/scala/s1/telegrambots/YourBot.scala
@@ -2,41 +2,38 @@ package s1.telegrambots
 import s1.telegrambots.BasicBot
 import scala.util.Using
 import scala.io.Source.fromURL
-import scala.io.Source
 import scala.util.Random
 
 
 object YourBot extends App:
     object Bot extends BasicBot:
 
-        private val pics: Vector[String] = Vector("1.png", "19.png", "40.png", "46.png", "92.png", "104.png", "105.png")
-        private val words: Vector[String] = Vector("Ronaldo", "CR7", "Cristiano Ronaldo", "Penaldo")
+        private val pics: Vector[String] = Vector("kuvat/1.png", "kuvat/19.png", "kuvat/40.png", "kuvat/46.png", "kuvat/92.png", "kuvat/104.png", "kuvat/105.png")
+        private val words: Vector[String] = Vector("ronaldo", "cr7", "cristiano ronaldo", "penaldo")
 
-        def ronaldo(msg: Message) =
-          val id = getChatId(msg)
-          val m = getString(msg)
-          if words.contains(m) then
-            sendPhoto(pics(Random.between(0,6)), id)
-            writeMessage("SIIUUU", id)
+        def doesMsgContainCR7(message: Message): Unit =
+          val ms = getString(message).toLowerCase()
+          println(ms)
+          if this.words.contains(ms) then
+            getRonaldoed(message)
 
-        /**
-         * TODO: Luokaa bottinne tähän metodeineen ja reagoijineen.
-         */
+        def getRonaldoed(message: Message) =
+          val id = getChatId(message)
+          sendPhoto(pics(Random.between(0,6)), id)
+          writeMessage("SIIUUU", id)
+
 
         this.onUserCommand("ruokalista", ruokalista)
 
-        def haeJSON() =
-          val json = fromURL(s"https://kitchen.kanttiinit.fi/menus?lang=fi&restaurants=&days=2023-10-4").mkString
-          json
-
         def ruokalista(message: Message): String = ???
+
+        this.onUserMessage(doesMsgContainCR7)
 
         this.run()
         // Tarkistetaan, että lähti käyntiin
         println("Started the bot")
 
     end Bot
-
     // Tämä rivi pyytää ja ajaa täten yllä olevan botin
     val bot = Bot 
 end YourBot
