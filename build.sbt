6d2f6d3ad4deee7809d7719f80d051e00dec0313
ollikolli <olli.a.korhonen@aalto.fi>
2023-10-04 13:28:47 +0300

    haeJSON metodi

--- build.sbt
+++ build.sbt
@@ -3,5 +3,6 @@ version:="0.3.0"
 scalaVersion := "3.1.1"
 
 
-libraryDependencies+= ("com.bot4s" %% "telegram-core" % "5.4.2").cross(CrossVersion.for3Use2_13)
+libraryDependencies += ("com.bot4s" %% "telegram-core" % "5.4.2").cross(CrossVersion.for3Use2_13)
 libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.9" % Test
+
--- src/main/scala/s1/telegrambots/YourBot.scala
+++ src/main/scala/s1/telegrambots/YourBot.scala
@@ -1,7 +1,8 @@
 package s1.telegrambots
 import s1.telegrambots.BasicBot
 import scala.util.Using
-import scala.io.Source
+import scala.io.Source.fromURL
+
 
 object YourBot extends App:
     object Bot extends BasicBot:
@@ -9,6 +10,14 @@ object YourBot extends App:
         /**
          * TODO: Luokaa bottinne tähän metodeineen ja reagoijineen.
          */
+
+        this.onUserCommand("ruokalista", ruokalista)
+
+        def haeJSON() =
+          val json = fromURL(s"https://kitchen.kanttiinit.fi/menus?lang=fi&restaurants=&days=2023-10-4").mkString
+          json
+
+        def ruokalista(message: Message): String = ???
 
         this.run()
         // Tarkistetaan, että lähti käyntiin
